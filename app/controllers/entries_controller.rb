feec47e9dcfb4ce0d7fd0720b9868e5edc7afef9
Kamil Kowalski <kowalski.tychy@gmail.com>
2016-05-22 11:12:11 +0200

    Add line breaks

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,9 +2,11 @@ class EntriesController < ApplicationController
     def index 
         @entries = Entry.order(date: :asc)
     end
+    
     def show 
          @entry = Entry.find(params["id"])
     end
+    
     def new 
         
     end
